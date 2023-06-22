f4de8ec99bb4ffb463a3da04f02a88d319e1edd8
fdrive422 <fafrago@gmail.com>
2023-06-21 21:00:19 -0700

    create inital dashboard layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,36 @@
-import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Box, CssBaseline, ThemeProvider } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
 import { useMemo } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { themeSettings } from "./theme";
+import Navbar from "@/scenes/navbar";
 
 function App() {
 	const theme = useMemo(() => createTheme(themeSettings), []);
 	return (
 		<div className="app">
-			<ThemeProvider theme={theme}>
-				<CssBaseline />
-			</ThemeProvider>
+			<BrowserRouter>
+				<ThemeProvider theme={theme}>
+					<CssBaseline />
+					<Box
+						width="100%"
+						height="100%"
+						padding="1rem 2rem 4rem 2rem"
+					>
+						<Navbar />
+						<Routes>
+							<Route
+								path="/"
+								element={<div>dashboard page </div>}
+							/>
+							<Route
+								path="/predictions"
+								element={<div>predictions page </div>}
+							/>
+						</Routes>
+					</Box>
+				</ThemeProvider>
+			</BrowserRouter>
 		</div>
 	);
 }
