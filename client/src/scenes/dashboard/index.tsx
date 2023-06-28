cc90175b10fe6fea7151c72fb867244db741c662
fdrive422 <fafrago@gmail.com>
2023-06-27 18:04:46 -0700

    build dash charts

--- client/src/scenes/dashboard/index.tsx
+++ client/src/scenes/dashboard/index.tsx
@@ -1,51 +1,51 @@
-import { Box, useMediaQuery, useTheme } from "@mui/material";
+import { Box, useMediaQuery } from "@mui/material";
 import Row1 from "./Row1";
 import Row2 from "./Row2";
 import Row3 from "./Row3";
 
 const gridTemplateLargeScreens = `
-"a b c"
-"a b c"
-"a b c"
-"a b f"
-"d e f"
-"d e f"
-"d h i"
-"g h i"
-"g h j"
-"g h j"
+	"a b c"
+	"a b c"
+	"a b c"
+	"a b f"
+	"d e f"
+	"d e f"
+	"d h i"
+	"g h i"
+	"g h j"
+	"g h j"
 `;
 const gridTemplateSmallScreens = `
-"a"
-"a"
-"a"
-"a"
-"b"
-"b"
-"b"
-"b"
-"c"
-"c"
-"c"
-"d"
-"d"
-"d"
-"e"
-"e"
-"f"
-"f"
-"f"
-"g"
-"g"
-"g"
-"h"
-"h"
-"h"
-"h"
-"i"
-"i"
-"j"
-"j"
+	"a"
+	"a"
+	"a"
+	"a"
+	"b"
+	"b"
+	"b"
+	"b"
+	"c"
+	"c"
+	"c"
+	"d"
+	"d"
+	"d"
+	"e"
+	"e"
+	"f"
+	"f"
+	"f"
+	"g"
+	"g"
+	"g"
+	"h"
+	"h"
+	"h"
+	"h"
+	"i"
+	"i"
+	"j"
+	"j"
 `;
 
 const Dashboard = () => {
@@ -59,8 +59,9 @@ const Dashboard = () => {
 			sx={
 				isAboveMediumScreens
 					? {
-							gridTempleColumns: "repeat(3, minmax(370px, 1fr))",
-							gridTempleRows: "repeat (10, minmax(60px, 1fr))",
+							gridTemplateColumns:
+								"repeat(3, minmax(370px, 1fr))",
+							gridTemplateRows: "repeat(10, minmax(60px, 1fr))",
 							gridTemplateAreas: gridTemplateLargeScreens,
 					  }
 					: {
--- server/index.js
+++ server/index.js
@@ -6,8 +6,12 @@ import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
 import kpiRoutes from "./routes/kpi.js";
+import productRoutes from "./routes/product.js";
+import transactionRoutes from "./routes/transaction.js";
 import KPI from "./models/KPI.js";
-import { kpis } from "./data/data.js";
+import Product from "./models/Product.js";
+import Transaction from "./models/Transaction.js";
+import { kpis, products, transactions } from "./data/data.js";
 
 /* CONFIGURATIONS */
 dotenv.config();
@@ -20,10 +24,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
-// console.log("hello");
-
 /* ROUTES */
 app.use("/kpi", kpiRoutes);
+app.use("/product", productRoutes);
+app.use("/transaction", transactionRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 9000;
@@ -35,8 +39,10 @@ mongoose
 	.then(async () => {
 		app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
-		/* UNCOMMENT AS NEEDED TO ADD DATA */
-		await mongoose.connection.db.dropDatabase();
-		KPI.insertMany(kpis);
+		/* ADD DATA ONE TIME ONLY OR AS NEEDED */
+		// await mongoose.connection.db.dropDatabase();
+		// KPI.insertMany(kpis);
+		// Product.insertMany(products);
+		// Transaction.insertMany(transactions);
 	})
 	.catch((error) => console.log(`${error} did not connect`));
