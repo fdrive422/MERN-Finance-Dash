a830b079198f7d15971e754aa8abb1968608bf81
fdrive422 <fafrago@gmail.com>
2023-07-02 12:02:51 -0700

    update import syntx

--- client/src/scenes/dashboard/Row2.tsx
+++ client/src/scenes/dashboard/Row2.tsx
@@ -3,7 +3,7 @@ import DashboardBox from "@/components/DashboardBox";
 import FlexBetween from "@/components/FlexBetween";
 import { useGetKpisQuery, useGetProductsQuery } from "@/state/api";
 import { Box, Typography, useTheme } from "@mui/material";
-import React, { useMemo } from "react";
+import { useMemo } from "react";
 import {
   Tooltip,
   CartesianGrid,
@@ -134,6 +134,7 @@ const Row2 = () => {
               dataKey="value"
             >
               {pieData.map((entry, index) => (
+              // {pieData.map((entry, index) => (
                 <Cell key={`cell-${index}`} fill={pieColors[index]} />
               ))}
             </Pie>
